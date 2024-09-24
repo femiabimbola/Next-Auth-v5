83c64d39f2d377d4bb453bc5fed039506efc8c92
Femi Abimbola <femiabimbola6@gmail.com>
2024-09-24 17:49:33 +0100

    adding middleware and auth config

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import authConfig from "./auth.config";
+import NextAuth from "next-auth";
+import { NextRequest } from "next/server";
+
+// Use only one of the two middleware options below
+// 1. Use middleware directly
+// export const { auth: middleware } = NextAuth(authConfig)
+
+// 2. Wrapped middleware option
+const { auth } = NextAuth(authConfig);
+
+// the middlware just calls the function above, ensuring all the routes are called
+export default auth(async function middleware(req: NextRequest) {
+  // Your custom middleware logic goes here
+  const login = !req;
+  console.log("route ->", login);
+  // console.log("route ->", req.nextUrl.pathname);
+});
